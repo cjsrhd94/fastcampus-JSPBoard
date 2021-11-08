3db5e96f73a6c51c427805472ef356fc26ba39f0
cjsrhd94 <cjsrhd94@gmail.com>
2021-11-08 21:19:30 +0900

    refactor: 글 수정 기능 이전 (updateBoard_proc.jsp -> DispatcherServlet)

--- src/com/fastcampus/controller/DispatcherServlet.java
+++ src/com/fastcampus/controller/DispatcherServlet.java
@@ -84,6 +84,24 @@ public class DispatcherServlet extends HttpServlet {
 
         } else if (path.equals("/updateBoard.do")) {
             System.out.println("글 수정 기능 처리");
+
+            // 1. 사용자 입력정보 추출
+            String title = request.getParameter("title");
+            String seq = request.getParameter("seq");
+            String content = request.getParameter("content");
+
+            // 2. DB 연동 처리
+            BoardVO vo = new BoardVO();
+            vo.setTitle(title);
+            vo.setSeq(Integer.parseInt(seq));
+            vo.setContent(content);
+
+            BoardDAO boardDAO = new BoardDAO();
+            boardDAO.updateBoard(vo);
+
+            // 3. 화면 네비게이션
+            response.sendRedirect("getBoardList.do");
+
         } else if (path.equals("/deleteBoard.do")) {
             System.out.println("글 삭제 기능 처리");
         } else if (path.equals("/getBoard.do")) {
--- web/getBoard.jsp
+++ web/getBoard.jsp
@@ -20,7 +20,7 @@
 <center>
     <h1>게시 글 상세</h1>
     <hr>
-    <form action="updateBoard_proc.jsp" method="post">
+    <form action="updateBoard.do" method="post">
         <input name="seq" type="hidden" value="<%=board.getSeq()%>"/>
         <table border="1" cellpadding="0" cellspacing="0">
             <tr>
@@ -55,9 +55,9 @@
     <%
         if (user.getRole().equals("ADMIN")){
     %>
-    <a href="deleteBoard_proc.jsp?seq=<%=board.getSeq()%>">글삭제</a>&nbsp;&nbsp;&nbsp;
+    <a href="deleteBoard.do?seq=<%=board.getSeq()%>">글삭제</a>&nbsp;&nbsp;&nbsp;
     <%}%>
-    <a href="getBoardList.jsp">글목록</a>
+    <a href="getBoardList.do">글목록</a>
 </center>
 </body>
 </html>
